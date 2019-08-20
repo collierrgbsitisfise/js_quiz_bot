8972ba700c8cdfac62c384f5ab3f05456a3e4205
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2019-08-20 23:36:26 +0300

    parse answe variants as well

--- app.js
+++ app.js
@@ -8,15 +8,28 @@ const regExpForJSCode = /\```javascript\n(((?:\r|\n|.)+))\```/g
 const regExpForQuestion = /[\.\s](((?:\r|.)+))\n/g;
 const regExpDetailsText = /<details>(((?:\r|\n|.)+))<\/details>/g;
 const regExpasnwerEXplanation = /<p>(((?:\r|\n|.)+))<\/p>/g;
+
 data.shift();
+
 const parsed = data.map(d => {
     const matchQuestion = d.match(regExpForQuestion);
     const matchesJSCODE = d.match(regExpForJSCode);
-    const matchAnseer = d.split('\n').filter(line => line.includes('Answer:'));
+
+    const dataSplitedByNewLine = d.split('\n');
+    const answerVariants = dataSplitedByNewLine.filter(line => {
+        const regExp = new RegExp("^(- [A|B|C|D])", "i");
+        return regExp.test(line);
+    });
+
+    console.log('Answer variants');
+    console.log(answerVariants);
+
+    const matchAnseer = dataSplitedByNewLine.filter(line => line.includes('Answer:'));
     const detailsText = d.match(regExpDetailsText);
     const answerExplanation = detailsText[0].match(regExpasnwerEXplanation);
 
     return {
+        answerVariants,
         question: matchQuestion[0],
         jsCode: matchesJSCODE && matchesJSCODE[0],
         answer: matchAnseer[0],

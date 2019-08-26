b97096df62e867389f48ce17dc96cf78547b25ee
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2019-08-26 13:17:51 +0300

    config nodemon for dev start

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,9 @@ import TelegramBot from 'node-telegram-bot-api';
 import { startHandler, getQuestionHandler, answerHandler } from './handlers';
 import { Redis } from './services';
 
-const client = new Redis('localhost', 6379, (err: Error) => console.log('REDIS ERROR : ', err));
-
 const botToken = process.env.tel_bot_token;
 
+const client = new Redis('localhost', 6379, (err: Error) => console.log('REDIS ERROR : ', err));
 const bot = new TelegramBot(botToken, { polling: true });
 
 bot.onText(/\/start/, startHandler(bot));
